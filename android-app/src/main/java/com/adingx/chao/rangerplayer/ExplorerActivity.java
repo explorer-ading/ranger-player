3c7815a452a6ac9b8b4308080c392e171560bd1b
explorer-ading <explorer.ading@yahoo.com>
2019-02-26 23:59:26 +0800

    explorer update

--- android-app/src/main/java/com/adingx/chao/rangerplayer/ExplorerActivity.java
+++ android-app/src/main/java/com/adingx/chao/rangerplayer/ExplorerActivity.java
@@ -1,7 +1,9 @@
 package com.adingx.chao.rangerplayer;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,23 +11,73 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExplorerActivity extends Activity {
 
     private static final String TAG = ExplorerActivity.class.getSimpleName();
     private ListView listView;
 
     // Defined Array values to show in ListView
-    private String[] listValues = new String[] {
-            "..",
-            "test.mp4"
-    };
+    private ArrayList<String> listValues = new ArrayList<String>();
 
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+/*
+    private List<File> getListFiles(File parentDir) {
+        ArrayList<File> inFiles = new ArrayList<File>();
+        File[] files = parentDir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                //inFiles.addAll(getListFiles(file));
+                inFiles.add(file);
+            } else {
+                //if(file.getName().endsWith(".csv")){
+                    inFiles.add(file);
+                //}
+            }
+        }
+        return inFiles;
+    }
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explorer);
 
         Log.i(TAG, "onCreate ~~");
+        String path;
+        listValues.add("..");
+        //ArrayList<String> mFileNames;
+        if (isExternalStorageReadable())    {
+            path = Environment.getExternalStorageDirectory().toString() + File.separator + Environment.DIRECTORY_DOWNLOADS;
+            Log.i(TAG, "Download path: " + path);
+            File file = new File(path);
+            //String mRootPath = file.getAbsoluteFile().getPath();
+            //mFileNames = new ArrayList<String>();
+
+            File filesInDirectory[] = file.listFiles();
+            if (filesInDirectory != null) {
+                Log.i(TAG, "files numbs: " + filesInDirectory.length);
+                for (int i = 0; i<filesInDirectory.length;i++) {
+                    //mFileNames.add(filesInDirectory[i].getName());
+                    listValues.add(filesInDirectory[i].getName());
+                    Log.i(TAG, "file: " + filesInDirectory[i].getName());
+                }
+            }else {
+                Log.i(TAG, "no files in path: " + path);
+            }
+        }
+
         listView = (ListView)findViewById(R.id.listView2);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, listValues);
@@ -52,5 +104,30 @@ public class ExplorerActivity extends Activity {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(getApplicationContext(), "permission was granted", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
 
 }
