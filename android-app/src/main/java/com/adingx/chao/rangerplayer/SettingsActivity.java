203603cde3f9433deae8484a2c42930eb0611641
explorer-ading <explorer.ading@yahoo.com>
2019-03-04 10:08:55 +0800

    v0.0.7 finish.

--- android-app/src/main/java/com/adingx/chao/rangerplayer/SettingsActivity.java
+++ android-app/src/main/java/com/adingx/chao/rangerplayer/SettingsActivity.java
@@ -1,10 +1,13 @@
 package com.adingx.chao.rangerplayer;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
 
 public class SettingsActivity extends AppCompatActivity {
     private static final String TAG = SettingsActivity.class.getSimpleName();
@@ -18,7 +21,20 @@ public class SettingsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = (TextView) findViewById(R.id.text_message);
+        TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+
+        String version = "v0.0.0";
+        version = "VERSION: " + BuildConfig.VERSION_NAME ;
+        /*
+        try {
+            PackageInfo pInfo = context.getPackageManager().getPackageInfo(getPackageName(), 0);
+            version = pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        */
+        TextView versionView = findViewById(R.id.text_version);
+        versionView.setText(version);
     }
 }
